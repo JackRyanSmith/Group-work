bc0ad857dee6022fa843aec6714d742ac85856b9
dannyhoover <dofjrtd@gmail.com>
2020-08-25 21:36:28 -0700

    final page

--- script.js
+++ script.js
@@ -27,9 +27,8 @@ async function displaySports(city) {
             const teamItem = $("<div>").appendTo(teamInput);
             const teamBody = $("<div>").addClass("#team-card-body").appendTo(teamItem);
             const teamForm = $("<form>").attr("id", "formId").appendTo(teamBody);
-            const submitBtn = $("<button>Submit</button>").addClass("#team-submit-btn").appendTo(teamForm);
             for (var i = 0; i < teams.data.length; i++) {
-                const teamFormItem = $("<input>").attr("type", "checkbox").attr("name", "team" + [i]).attr("value", `${teams.data[i].name}`);
+                const teamFormItem = $("<input>").attr("class", "choseTeam").attr("type", "checkbox").attr("name", "team" + [i]).attr("value", `${teams.data[i].name}`);
                 const teamFormLabel = $("<label>").attr("for", "team" + [i]).text(`${teams.data[i].name}`);
                 const breakLine = $("<br>");
                 teamForm.append(teamFormItem, teamFormLabel, breakLine);
@@ -37,17 +36,44 @@ async function displaySports(city) {
             // $("#formId").on("click", function() {
             //     console.log($(`input[type=checkbox][name=${teams.data[i]}]:checked`).val());
             // });
+            const submitBtn = $("<button>Submit</button>").addClass("#team-submit-btn").appendTo(teamForm);
             submitBtn.on("click", function (event) {
                 event.preventDefault();
-                console.log(event.parent());
+                var teamArray = [];
+                $(".choseTeam").each(function (index, item) {
+                    console.log(index + " = " + item.checked + " = " + item.value);
+                    if (item.checked == true){
+                        teamArray.push(item.value);
+                    }
+                });
+                localStorage.setItem("teamArray", teamArray);
                 teamItem.addClass("hide");
-                const optionsList = $("<ul>").appendTo(options);
-                const optionOne = $("<li>").text("Next game");
-                const optionTwo = $("<li>").text("Team schedule");
-                const optionThree = $("<li>").text("Team standings");
-                const optionFour = $("<button>Submit</button>");
-                optionsList.append(optionOne, optionTwo, optionThree, optionFour);
 
+                const breakLine = $("<br>");
+                const optionsForm = $("<form>").appendTo(options);
+                const optionItemOne = $("<input>").attr("class", "choose-option").attr("type", "checkbox").attr("name", "option-1");
+                const optionOneLabel = $("<label>").attr("for", "option-1").text("Next Game").append($("<br>"));
+                const optionItemTwo = $("<input>").attr("class", "choose-option").attr("type", "checkbox").attr("name", "option-2");
+                const optionTwoLabel = $("<label>").attr("for", "option-2").text("Team Schedule").append($("<br>"));
+                const optionItemThree = $("<input>").attr("class", "choose-option").attr("type", "checkbox").attr("name", "option-3");
+                const optionThreeLabel = $("<label>").attr("for", "option-3").text("Team Standings").append($("<br>"));
+                optionsForm.append(optionItemOne, optionOneLabel, optionItemTwo, optionTwoLabel, optionItemThree, optionThreeLabel);
+                
+
+                const submitBtn2 = $("<button>Submit</button>").addClass("#option-submit-btn").appendTo(optionsForm);
+                submitBtn2.on("click", function(event){
+                    event.preventDefault();
+                    var optionArray = [];
+                    $(".choose-option").each(function (index, item){
+                        if (item.checked == true){
+                            optionArray.push(item.value);
+                        }
+                    });
+                    localStorage.setItem("optionArray", optionArray);
+                    optionsForm.addClass("hide");
+
+                    // last page functionality
+                })
             })
         }
         // error checks
@@ -57,95 +83,73 @@ async function displaySports(city) {
 }
 function displayTeams(city) {
 }
-
 // function renderTeamList() {
 //     // Clear todoList element and update todoCountSpan
 //     teamList.innerHTML = "";
 //     teamCountSpan.textContent = teams.length;
-
 //       // Render a new li for each todo
 //   for (var i = 0; i < teams.length; i++) {
 //             var team = teams[i];
-
 //             var li = document.createElement("li");
 //             li.textContent = team;
 //             li.setAttribute("data-index", i);
-
 //             var button = document.createElement("button");
 //             button.textContent = "Complete";
-
 //             li.appendChild(button);
 //             todoList.appendChild(li);
 //         }
 // }
-
 // function init() {
 //   // Get stored todos from localStorage
 //   // Parsing the JSON string to an object
 //   var storedTeams = JSON.parse(localStorage.getItem("teams"));
-
 //   // If todos were retrieved from localStorage, update the todos array to it
 //   if (storedTeams !== null) {
 //     teams = storedTeams;
 //   }
-
 //   // Render todos to the DOM
 //   renderTodos();
 // }
-
 // function storeTodos() {
 //   // Stringify and set "todos" key in localStorage to todos array
 //   localStorage.setItem("teams", JSON.stringify(teams));
 // }
-
 // // When form is submitted...
 // todoForm.addEventListener("submit", function(event) {
 //   event.preventDefault();
-
 //   var teamText = teamInput.value.trim();
-
 //   // Return from function early if submitted todoText is blank
 //   if (teamText === "") {
 //     return;
 //   }
-
 //   // Add new todoText to todos array, clear the input
 //   teams.push(todoText);
 //   teamInput.value = "";
-
 //   // Store updated todos in localStorage, re-render the list
 //   storeTeams();
 //   renderTeams();
 // });
-
 // Run our AJAX call to get teams
 // $.ajax({
 // url: cityQueryURL,
 // method: "GET"
 // }).then(function(response) {
-
 //     for (var i = 0; i < response.data.length; i++) {
 //         if (response.data[i].leagueId === "111") {
 //             console.log("Upcoming " + response.data[i].name + " games:");
 //             teamName = response.data[i].name
 //         }
 //     }
-
 // });
-
 // Run our AJAX call to get the upcoming game schedule
 // $.ajax({
 // url: gamesQueryURL,
 // method: "GET"
 // }).then(function(response) {
-
 //     var game = response.data;
-
 //     for (var i = 0; i < game.length; i++) {
 //         if ((game[i].awayName == teamName || game[i].homeName == teamName) && (new Date(game[i].matchTime*1000) > new Date())) {
 //             console.log(new Date(game[i].matchTime*1000));
 //         }
 //     }
-
 // });
-
