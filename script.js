f781b138f8d42f758ef9a50fb3d52d5084197edb
dannyhoover <dofjrtd@gmail.com>
2020-08-27 19:44:00 -0700

    Post tutoring, styling update, new API for header
    Co-authored-by: Wesley Clements <wesley.clements@gmail.com>

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Danny's iSports API key: DaMMhwibfDPKxlHn
-// Christian's iSports API key: 0uzEF34YYjTbHstM
+// Christian's iSports API key: 0uzEF34YYjTbHstM; Pexels API key: 563492ad6f91700001000001fc2a789fa0864115a147ac0879147312
 // JackRyan's isport API key: wFO2Vj7ud4ZRBpCa
 // Michael's iSports API key: OpV33oLoEsvyd08B
 const searchForm = $("#city-search-form");
@@ -7,133 +7,160 @@ const searchInput = $("#search-city");
 const teamInput = $("#team-selection");
 const options = $("#info-options");
 const finalResults = $("#results");
-const headerText = $("#header-id");
 
-// starting page search box and next page listener
 searchForm.on("submit", function (event) {
     event.preventDefault();
     const cityName = $("#search-city").val();
     if (!cityName) return;
     searchForm.addClass("hide");
-    displaySports(cityName);
+    $("#header").text(cityName);
+    // Pexels - photo search
+    var apiKey = "563492ad6f91700001000001fc2a789fa0864115a147ac0879147312"
+    $.ajax({
+        method: "GET",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", apiKey);
+        },
+        url: "https://api.pexels.com/v1/search?query=" + cityName,
+    }).then(function (data) {
+        var cityImage = (data.photos[0].src.original);
+        console.log(cityImage);
+        $("#header").css({
+            "background-image": "url(" + cityImage + ")",
+            "background-position": "center"
+        });
+    });
+    searchForTeamData(cityName);
 });
+
+
 // Building the URL we need to query the iSports database
-async function displaySports(city) {
+async function searchForTeamData(city) {
     try {
         const responses = await Promise.all([
             fetch(`https://cors-anywhere.herokuapp.com/http://api.isportsapi.com/sport/basketball/team/search?api_key=DaMMhwibfDPKxlHn&name=${city}`),
             fetch(`https://cors-anywhere.herokuapp.com/http://api.isportsapi.com/sport/basketball/schedule?api_key=DaMMhwibfDPKxlHn&leagueId=111`)
         ]);
 
-        const [teams, games] = await Promise.all(responses.map(response => response.json()));
-        console.log(teams, games);
-        displayTeams(teams);
-
-        // grab next game data
-        for (var i = 0; i < teams.data.length; i++) {
-            if (teams.data[i].leagueId === "111") {
-                console.log("Upcoming " + teams.data[i].name + " games:");
-                teamName = teams.data[i].name
-            }
-        }
-        var game = games.data;
-        for (var i = 0; i < game.length; i++) {
-            if ((game[i].awayName == teamName || game[i].homeName == teamName) && (new Date(game[i].matchTime * 1000) > new Date())) {
-                console.log(new Date(game[i].matchTime * 1000));
-            }
-        }
-        // end of grab next game data
-
-        // replacing header text with user picked city
-        const removeHeader = document.getElementById("replace-header");
-        removeHeader.remove();
-        const headerCity = $("<h1>").text(`${$("#search-city").val()}`);
-        headerText.append(headerCity);
-
-        // appending local team data
-        const teamItem = $("<div>").appendTo(teamInput); // teamInput < teamItem < teamBody < teamForm < divTemp < teamFormItem + teamFormLabel + breakLine
-        const teamBody = $("<div>").addClass("#team-card-body").appendTo(teamItem);
-        const teamForm = $("<form>").attr("id", "formId").appendTo(teamBody);
-
-        // loop that gives labels/checkboxes to teams no matter how long the list is
-        for (var i = 0; i < teams.data.length; i++) {
-            const divTemp = $("<div>");
-            const teamFormItem = $("<input>").attr("class", "choseTeam").attr("type", "checkbox").attr("id", "team" + [i]).attr("name", "team" + [i]).attr("value", `${teams.data[i].name}`).appendTo(divTemp);
-            const teamFormLabel = $("<label>").attr("for", "team" + [i]).text(`${teams.data[i].name}`).appendTo(divTemp);
-            const breakLine = $("<br>").appendTo(divTemp);
-            teamForm.append(divTemp);
-        }
-        const submitBtn = $("<button>Submit</button>").attr("id", "submit-btn").addClass("btn waves-effect waves-light").attr("type", "submit").attr("name", "action").appendTo(teamForm);
-
-        // click listener that leads to next page with info list
-        submitBtn.on("click", function (event) {
-            event.preventDefault();
-            var teamArray = [];
-
-            $(".choseTeam").each(function (index, item) {
-                console.log(index + " = " + item.checked + " = " + item.value);
-                if (item.checked == true){
-                    teamArray.push(item.value);
-                }
-            });
-
-            localStorage.setItem("teamArray", teamArray);
-            teamItem.addClass("hide");
-
-            const optionItem = $("<div>").appendTo(options); // options < optionItem < optionBody < optionsForm < divTemp <
-            const optionBody = $("<div>").addClass("#option-card-body").appendTo(optionItem);
-            const optionsForm = $("<form>").attr("id", "formId").appendTo(optionBody);
-
-            const divTemp1 = $("<div>");
-            const optionItemOne = $("<input>").attr("class", "choose-option").attr("type", "checkbox").attr("id", "option-1").attr("name", "option-1").attr("value", "Next Game").appendTo(divTemp1);
-            const optionOneLabel = $("<label>").attr("for", "option-1").text("Next Game").appendTo(divTemp1);
-            const breakLine1 = $("<br>").appendTo(divTemp1);
-
-            const divTemp2 = $("<div>");
-            const optionItemTwo = $("<input>").attr("class", "choose-option").attr("type", "checkbox").attr("id", "option-2").attr("name", "option-2").attr("value", "Team Schedule").appendTo(divTemp2);
-            const optionTwoLabel = $("<label>").attr("for", "option-2").text("Team Schedule").appendTo(divTemp2);
-            const breakLine2 = $("<br>").appendTo(divTemp2);
-
-            const divTemp3 = $("<div>");
-            const optionItemThree = $("<input>").attr("class", "choose-option").attr("type", "checkbox").attr("id", "option-3").attr("name", "option-3").attr("value", "Team Standings").appendTo(divTemp3);
-            const optionThreeLabel = $("<label>").attr("for", "option-3").text("Team Standings").appendTo(divTemp3);
-            const breakLine3 = $("<br>").appendTo(divTemp3);
-
-            optionsForm.append(divTemp1, divTemp2, divTemp3);
-            
-
-            const submitBtn2 = $("<button>Submit</button>").attr("id", "submit-btn").addClass("btn waves-effect waves-light").attr("type", "submit").attr("name", "action").appendTo(optionsForm);
-
-            submitBtn2.on("click", function(event){
-                event.preventDefault();
-                var optionArray = [];
-
-                $(".choose-option").each(function (index, item){
-                    console.log(index + " = " + item.checked + " = " + item.value);
-                    if (item.checked == true){
-                        optionArray.push(item.value);
-                    }
-                });
-
-                localStorage.setItem("optionArray", optionArray);
-                optionsForm.addClass("hide");
-
-                for (var i = 0; i < teamArray.length; i++) {
-                    const teamData = $("<div>").text(teamArray[i]).appendTo(finalResults);
-                    for (var j = 0; j < optionArray.length; j++) {
-                        const optionsData = $("<div>").text(optionArray[j]).appendTo(teamData)
-                    }
-                    teamData.append($("<br>"));
-                };
-            });
-
+        const [teamSearchResults, schedule] = await Promise.all(responses.map(response => response.json()));
+        
+        searchForm.addClass("hide");
+        
+        teams = teamSearchResults.data.map(function(teamData) {
+            // Filter object properties
+            return { ...teamData };
         });
+        matches = schedule.data.map(function(matchData) {
+            // Filter object properties
+            return { ...matchData };
+        });
+        console.log(teams, matches);
+        for (var i = 0; i < matches.length; i++) {
+            if (matches[i].homeId == 6){
+                console.log(matches[i].matchTime);
+            }
+        }
+
+        displayTeamSelection();
         
         // error checks
     } catch (error) {
         console.error(error);
     }
 }
-function displayTeams(city) {
+
+
+function displayTeamSelection() {
+    // appending local team data
+    const teamItem = $("<div>").appendTo(teamInput); // teamInput < teamItem < teamBody < teamForm < divTemp < teamFormItem + teamFormLabel + breakLine
+    const teamBody = $("<div>").addClass("#team-card-body").appendTo(teamItem);
+    const teamForm = $("<form>").attr("id", "formId").appendTo(teamBody);
+
+    // loop that gives labels/checkboxes to teams no matter how long the list is
+    for (var i = 0; i < teams.length; i++) {
+        const divTemp = $("<div>");
+        const teamFormItem = $("<input>").attr("class", "choseTeam").attr("type", "checkbox").attr("id", "team" + [i]).attr("name", "team" + [i]).attr("value", `${teams[i].name}`).appendTo(divTemp);
+        const teamFormLabel = $("<label>").attr("for", "team" + [i]).text(`${teams[i].name}`).appendTo(divTemp);
+        const breakLine = $("<br>").appendTo(divTemp);
+        teamForm.append(divTemp);
+    }
+    const submitBtn = $("<button>Submit</button>").attr("id", "submit-btn").addClass("btn waves-effect waves-light").attr("type", "submit").attr("name", "action").appendTo(teamForm);
+
+    // click listener that leads to next page with info list
+    submitBtn.on("click", function (event) {
+        event.preventDefault();
+
+        $(".choseTeam").each(function (index, item) {
+            if (!item.checked) delete teams[index];
+        });
+
+        teams = teams.filter(function(team) {
+            return team;
+        });
+
+        teamItem.addClass("hide");
+
+        displayDataSelection();
+    });
 }
 
+function displayDataSelection() {
+    const optionItem = $("<div>").appendTo(options); // options < optionItem < optionBody < optionsForm < divTemp <
+    const optionBody = $("<div>").addClass("#option-card-body").appendTo(optionItem);
+    const optionsForm = $("<form>").attr("id", "formId").appendTo(optionBody);
+
+    const divTemp1 = $("<div>");
+    const optionItemOne = $("<input>").attr("class", "choose-option").attr("type", "checkbox").attr("id", "option-1").attr("name", "option-1").appendTo(divTemp1);
+    const optionOneLabel = $("<label>").attr("for", "option-1").text("Next Game").appendTo(divTemp1);
+    const breakLine1 = $("<br>").appendTo(divTemp1);
+
+    const divTemp2 = $("<div>");
+    const optionItemTwo = $("<input>").attr("class", "choose-option").attr("type", "checkbox").attr("id", "option-2").attr("name", "option-2").appendTo(divTemp2);
+    const optionTwoLabel = $("<label>").attr("for", "option-2").text("Team Schedule").appendTo(divTemp2);
+    const breakLine2 = $("<br>").appendTo(divTemp2);
+
+    const divTemp3 = $("<div>");
+    const optionItemThree = $("<input>").attr("class", "choose-option").attr("type", "checkbox").attr("id", "option-3").attr("name", "option-3").appendTo(divTemp3);
+    const optionThreeLabel = $("<label>").attr("for", "option-3").text("Team Standings").appendTo(divTemp3);
+    const breakLine3 = $("<br>").appendTo(divTemp3);
+
+    optionsForm.append(divTemp1, divTemp2, divTemp3);
+    
+    const submitBtn2 = $("<button>Submit</button>").attr("id", "submit-btn").addClass("btn waves-effect waves-light").attr("type", "submit").attr("name", "action").appendTo(optionsForm);
+
+    submitBtn2.on("click", function(event){
+        event.preventDefault();
+        optionsForm.addClass("hide");
+
+        displayData(optionItemOne.is(":checked"), optionItemTwo.is(":checked"), optionItemThree.is(":checked"))
+    });
+}
+
+function displayData(nextGame, teamSchedule, teamStanding) {
+    console.log(nextGame, teamSchedule, teamStanding);
+    teams.forEach(function(team) {
+        console.log(team);
+        console.log(team.name);
+        const teamData = $("<div>").text(team.name).appendTo(finalResults);
+        if (nextGame) {
+            const firstMatch = matches.filter(function(match) {
+                console.log(match.homeName);
+                return ((match.homeName === team.name || match.awayName === team.name) && match.matchTime > Date.now());
+            });
+            const optionsData = $("<div>").text(firstMatch && new Date(firstMatch.matchTime*1000) || "none").appendTo(teamData)
+        }
+        if (teamSchedule) {
+            const teamMatches = matches.filter(function(match) {
+                return (match.homeName === team.name || match.awayName === team.name);
+            });
+            const optionsData = $("<div>").text(teamMatches).appendTo(teamData)
+        }
+        if (teamStanding) {
+            // another api
+            const optionsData = $("<div>").text("Team Standing").appendTo(teamData)
+        }
+        teamData.append($("<br>"));
+    });
+}
+
+// filter, find, map
